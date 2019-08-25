81aeb176603ea988a9566236448b7d13df3e81d8
Rafa <rafaelu05@gmail.com>
2019-08-25 11:23:57 -0300

    entidades padrão jdl

---
+++ src/main/java/br/com/angular/domain/enumeration/Language.java
@@ -0,0 +1,8 @@
+package br.com.angular.domain.enumeration;
+
+/**
+ * The Language enumeration.
+ */
+public enum Language {
+    FRENCH, ENGLISH, SPANISH
+}
---
+++ src/main/java/br/com/angular/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package br.com.angular.repository;
+
+import br.com.angular.domain.Employee;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Employee entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
